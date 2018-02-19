ebe9a5cb499afee92e7e96da4506b171c0fa3672
Yuki Tsuchida <d@gahaku.tech>
2018-02-19 06:13:21 +0000

    掲示板の投稿内容の表示まで

--- app.rb
+++ app.rb
@@ -3,7 +3,18 @@ Bundler.require
 require 'sinatra/reloader' if development?
 require 'open-uri'
 require 'sinatra/json'
+require './models/contribution.rb'
 
 get '/' do
+    @contents = Contribution.order('id desc').all
     erb :index
+end
+
+post '/new' do
+    Contribution.create({
+        name: params[:user_name],
+        body: params[:body]
+    })
+    
+    redirect '/'
 end
